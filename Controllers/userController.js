56a90df0791dd60102dae72493e3e4cdeca58f32
LeoAlexThomas <leoalex960@gmail.com>
2025-12-03 11:03:30 +0530

    added auth middlewares and also added restricted routes for getting user info

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -6,8 +6,11 @@ import jwt from "jsonwebtoken";
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
+    // Hashing the password
     const hashedPassword = await bcrypt.hash(password, 12);
+    // Creating new user
     const newUser = new User({ name, email, password: hashedPassword });
+    // Saving user in db
     await newUser.save();
     res.status(200).json({
       message: "User registered successfully",
@@ -24,23 +27,27 @@ export const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+    // Checking user is available in db
     if (!user) {
       res.status(404).json({
         message: "User email not found",
       });
     }
+    // Checking user password and stored password are same.
     const isValidPassword = await bcrypt.compare(password, user.password);
-
+    // If password is incorrect
     if (!isValidPassword) {
       res.status(401).json({
         message: "Password is incorrect",
       });
     }
 
+    // Generating jwt token with payload of userId adding secret to jwt token
     const jwtToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
 
+    // Storing the jwt token in user data
     user.token = jwtToken;
 
     await user.save();
@@ -54,6 +61,31 @@ export const loginUser = async (req, res) => {
   } catch (error) {
     res.status(503).json({
       message: "Cannot create user, Error in creating user",
+    });
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+    res.status(200).json({
+      data: req.user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Cannot get User data, Error in getting user info",
+    });
+  }
+};
+
+export const getAllUser = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Cannot get All user data, Error in getting all user info",
     });
   }
 };
--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { loginUser, registerUser } from "../Controllers/userController.js";
+import {
+  getAllUser,
+  getUser,
+  loginUser,
+  registerUser,
+} from "../Controllers/userController.js";
+import {
+  adminMiddleware,
+  authMiddleware,
+} from "../Middleware/customMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+// Added auth middleware for checking user is loggedIn. If loggedIn, user info will be added to request
+router.get("/getCurrentUser", authMiddleware, getUser);
+// Added auth middleware for checking user is loggedIn. If loggedIn, user info will be added to request and adding admin restriction to this route
+router.get("/getAllUsers", authMiddleware, adminMiddleware, getAllUser);
 
 export default router;
